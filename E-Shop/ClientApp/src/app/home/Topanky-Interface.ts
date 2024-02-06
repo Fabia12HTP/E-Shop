07944f1c0e45f86e09dffd5a6e56a5f353fa51d1
timotejbalajka@gmail.com <timotejbalajka@gmail.com>
2024-02-06 09:59:31 +0100

    DTO a migration

---
+++ E-Shop/ClientApp/src/app/home/Topanky-Interface.ts
@@ -0,0 +1,7 @@
+export interface Topanky {
+  name: string;
+  description: string;
+  rating: number;
+  urlpicture: string;
+  like: boolean;
+}
---
+++ E-Shop/DTO/Topanky.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Shop.DTO
+{
+    public class Topanky
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Rating  { get; set; }
+        public string UrlPicture { get; set; }
+        public bool Like { get; set; }
+    }
+}
