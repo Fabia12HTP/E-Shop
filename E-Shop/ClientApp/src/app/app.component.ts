38dc83d4e54aadb7c30a6f93f560c5029e040275
TomuVer <sim.private7@gmail.com>
2024-03-02 21:54:16 +0100

    7.4 Dark-Mode

--- E-Shop/ClientApp/src/app/app.component.ts
+++ E-Shop/ClientApp/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from './dark-mode/dark-mode.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  darkModeState = false;
+
+  constructor(private darkModeService: DarkModeService) { }
+
+  ngOnInit(): void {
+    this.darkModeService.getDarkModeState().subscribe(state => {
+      this.darkModeState = state;
+    });
+  }
+
+  toggleDarkMode(): void {
+    this.darkModeService.toggleDarkMode();
+  }
 }
--- E-Shop/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ E-Shop/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../dark-mode/dark-mode.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+  // Inicializujeme darkModeState na false
+  darkModeState = false;
+
+  constructor(private darkModeService: DarkModeService) {
+    // Pri inicializácii komponentu sa zaregistrujeme na observable
+    // a aktualizujeme stav dark mode
+    this.darkModeService.getDarkModeState().subscribe(state => {
+      this.darkModeState = state;
+    });
+  }
+
+  // Metóda na zmenu stavu isExpanded
   isExpanded = false;
 
   collapse() {
@@ -16,3 +29,4 @@ export class NavMenuComponent {
     this.isExpanded = !this.isExpanded;
   }
 }
+
